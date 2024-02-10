fbbcc6a33d96350fa0f90715cf25912783989838
Hz22818 <122125227+Hz22818@users.noreply.github.com>
2024-02-11 01:11:49 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,15 @@
+from aiogram import executor
+import time
+from loader import vip
+from colorama import Fore, Style
+import threading
+
+author = Fore.LIGHTRED_EX + Style.BRIGHT + "Made By " + Fore.LIGHTGREEN_EX + Style.BRIGHT + "@business_dark" + Fore.RESET + Style.NORMAL + '\n'
+start = Fore.LIGHTYELLOW_EX + Style.BRIGHT + "Бот успешно запущен!" + Fore.RESET + Style.NORMAL + '\n'
+
+if __name__ == '__main__':
+    time.sleep(0.03)
+    print(author, end='', flush=True)
+    time.sleep(0.03)
+    print(start, end='', flush=True)
+    executor.start_polling(vip)
